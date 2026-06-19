45d0411f490d683fef224005e1852b00b46a148f
F3rr4z-02 <hferraz.henrique@gmail.com>
2026-06-18 22:24:28 -0300

    Atualiza mensagem versao final

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('<h1>N3 DevOps - Apresentacao ao vivo! 2026</h1>');
+  res.send('<h1>N3 DevOps - Versao Final! 2026</h1>');
 });
 
 app.get('/health', (req, res) => {
